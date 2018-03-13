c901a29eeb48aca31245731d814af742e3329f94
root <root@DESKTOP-NDBA04D.localdomain>
2018-03-13 12:43:50 -0400

    removed test page for php

--- test.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $request = "https://api.themoviedb.org/3/movie/550?api_key=5d3e05e2d12a6b7a95fa4479a0894bfb";
-    $json = file_get_contents($request);
-    print_r(json_decode($json, true));
-?>
